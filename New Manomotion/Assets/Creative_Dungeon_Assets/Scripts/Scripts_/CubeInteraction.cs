8543e5a41e57abd2e351cba2e4f74f82434bdd84
ThePacerJ <vrromson@gmail.com>
2020-08-06 08:12:45 +0530

    updates on scene

---
+++ New Manomotion/Assets/Creative_Dungeon_Assets/Scripts/Scripts_/CubeInteraction.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeInteraction : MonoBehaviour
+{
+	bool isHighlighted;
+
+	//Feel free to choose your own colors
+	public ParticleSystem particleEffect;
+	public Color idleColor;
+	public Color isHighliughtedColor;
+	public AudioSource fireAudio;
+	MeshRenderer cubeMeshRenderer;
+	ParticleSystemRenderer particleRenderer;
+	AudioRenderer fireaudioRenderer;
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		particleRenderer = this.GetComponent<ParticleSystemRenderer>();
+		fireaudioRenderer = this.GetComponent<AudioRenderer>();
+		cubeMeshRenderer = this.GetComponent<MeshRenderer>();
+		isHighlighted = false;
+		UpdateColor();
+	}
+
+	/// <summary>
+	/// Updates the cubes material color according to the status (isActivated or Not)
+	/// </summary>
+	private void UpdateColor()
+	{
+		if (isHighlighted)
+		{
+			particleEffect.Play();
+			fireAudio.Play();
+			cubeMeshRenderer.material.color = isHighliughtedColor;
+		}
+		else
+		{
+			particleEffect.Stop();
+			fireAudio.Stop();
+			cubeMeshRenderer.material.color = idleColor;
+		}
+	}
+	
+	/// <summary>
+	/// Toggles the cubes activation value (isActive or Not)
+	/// </summary>
+	private void ToggleHighlight()
+	{
+		isHighlighted = !isHighlighted;
+	}
+
+	/// <summary>
+	/// The code logic of the interaction. In our example, Toggle the status and update the visuals.
+	/// </summary>
+	public void InteractWithCube()
+	{
+		ToggleHighlight();
+		UpdateColor();
+	}
+}
+
---
+++ New Manomotion/Assets/Creative_Dungeon_Assets/Scripts/Scripts_/InteractionManagerCube.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManagerCube : MonoBehaviour
+{
+	[SerializeField]
+	CubeInteraction currentInteractableCube;
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		// DetectMouseClick();
+		DetectHandGestureClick();
+	}
+
+
+	void DetectHandGestureClick()
+	{
+
+		//All the information of the hand
+		HandInfo detectedHand = ManomotionManager.Instance.Hand_infos[0].hand_info;
+
+		//The click happens when I perform the Click Gesture : Open Pinch -> Closed Pinch -> Open Pinch 
+		if (detectedHand.gesture_info.mano_gesture_trigger == ManoGestureTrigger.RELEASE_GESTURE)
+		{
+			//Logic that should happen when I click/release gesture
+			if (currentInteractableCube)
+			{
+				currentInteractableCube.InteractWithCube();
+
+			}
+		}
+
+	}
+
+
+	void DetectMouseClick()
+	{
+
+		//The click happens when I release the left mouse buttons
+		if (Input.GetMouseButtonUp(0))
+		{
+
+			//Logic that should happen when I click.
+
+			if (currentInteractableCube)
+			{
+				currentInteractableCube.InteractWithCube();
+
+			}
+
+		}
+	}
+}
